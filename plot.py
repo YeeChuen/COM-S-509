98e25222069822cca110e56a339331980400cf3c
chuen <tychuen98@gmail.com>
2023-11-27 22:52:55 -0600

    new data result

--- plot.py
+++ plot.py
@@ -12,6 +12,11 @@ def readfile(folder):
         return to_return
 
 def plot_bar_individual(file_content_list, save_name, dataset_name, model_type, combination = None):
+
+    dataset_labels = []
+    combination_test = {}
+    combination_train = {}
+
     labels = []
     legend = []
     for content in file_content_list:
@@ -35,13 +40,59 @@ def plot_bar_individual(file_content_list, save_name, dataset_name, model_type, 
     test = techniques_test[dataset_name]
     train = techniques_train[dataset_name]
 
+    # plot with combination if provided
+    if combination:
+        for content in combination:
+            if content[1] not in dataset_labels:
+                dataset_labels.append(content[1])
+            if content[1] not in combination_test:
+                combination_test[content[1]] = []
+            if content[1] not in combination_train:
+                combination_train[content[1]] = []
+            combination_test[content[1]].append(float(content[2])*100)
+            combination_train[content[1]].append(float(content[3])*100)
+
+        mean = {}
+        mean['test'] = {}
+        mean['train'] = {}
+
+        for dataset in dataset_labels:
+        #labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+            combination_test[dataset].sort()
+            combination_train[dataset].sort()
+            average_test = sum(combination_test[dataset]) / len(combination_test[dataset]) 
+            mean['test'][dataset] = average_test
+            average_train = sum(combination_train[dataset]) / len(combination_train[dataset]) 
+            mean['train'][dataset] = average_train
+        
+        #print(mean)
+        test.insert(1, mean['test'][dataset_name])
+        train.insert(1, mean['train'][dataset_name])
+        labels.insert(1, 'combination')
+
     x = np.arange(len(labels))  # the label locations
     width = 0.2  # the width of the bars
 
     fig, ax = plt.subplots(figsize=(10, 6))
+    if combination:
+        color_list_test = [] # blue
+        color_list_train = [] # orange
 
-    rects2 = ax.bar(x - 2*width/len(legend), test, width, label='Test Acc')
-    rects3 = ax.bar(x + 2*width/len(legend), train, width, label='Train Acc')
+        for i in range(len(test)):
+            if i == 1:
+                color_list_test.append('tab:blue')
+                color_list_train.append('tab:orange')
+
+            else:
+                color_list_test.append('lightsteelblue')
+                color_list_train.append('bisque')
+
+        rects2 = ax.bar(x - 2*width/len(legend), test, width, label='Test Acc', color = color_list_test)
+        rects3 = ax.bar(x + 2*width/len(legend), train, width, label='Train Acc', color = color_list_train)
+
+    else:
+        rects2 = ax.bar(x - 2*width/len(legend), test, width, label='Test Acc')
+        rects3 = ax.bar(x + 2*width/len(legend), train, width, label='Train Acc')
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Accuracy Score')
@@ -87,7 +138,6 @@ def plot_bar_combination(file_content_list, save_name, model_type):
         techniques_test[content[1]].append(float(content[2])*100)
         techniques_train[content[1]].append(float(content[3])*100)
 
-
     mean = {}
     mean['test'] = []
     mean['train'] = []
@@ -112,10 +162,6 @@ def plot_bar_combination(file_content_list, save_name, model_type):
         mean['train'].append(average_train)
         maxi['train'].append(techniques_train[technique][len(techniques_train[technique]) - 1] - average_train)
         mini['train'].append(average_train - techniques_train[technique][0])
-
-    #print(mean)
-    #print(maxi)
-    #print(mini)
 
     x = np.arange(len(labels))  # the label locations
     width = 0.2  # the width of the bars
@@ -181,11 +227,17 @@ if __name__ == "__main__":
     folder = 'example_LR_result.txt'
     file_content_list = readfile(folder)
     plot_bar_individual(file_content_list,'LR_Alzheimers_Handwriting', 'Alzheimers Handwriting', 'Binomial Logistic Regression')
-    plot_bar_individual(file_content_list,'LR_Breast Cancer', 'Breast Cancer', 'Binomial Logistic Regression')
-    plot_bar_individual(file_content_list,'LR_Spam Email', 'Spam Email', 'Binomial Logistic Regression')
-    plot_bar_individual(file_content_list,'LR_Water Potability', 'Water Potability', 'Binomial Logistic Regression')
+    plot_bar_individual(file_content_list,'LR_Breast_Cancer', 'Breast Cancer', 'Binomial Logistic Regression')
+    plot_bar_individual(file_content_list,'LR_Spam_Email', 'Spam Email', 'Binomial Logistic Regression')
+    plot_bar_individual(file_content_list,'LR_Water_Potability', 'Water Potability', 'Binomial Logistic Regression')
 
     
     folder_comb = 'example_LR_combination.txt'
-    file_content_list = readfile(folder_comb)
-    plot_bar_combination(file_content_list, 'LR_Combination', 'Binomial Logistic Regression')
+    result_list = readfile(folder_comb)
+    plot_bar_combination(result_list, 'LR_Combination', 'Binomial Logistic Regression')
+
+    
+    plot_bar_individual(file_content_list,'LR_Alzheimers_Handwriting_w_combination', 'Alzheimers Handwriting', 'Binomial Logistic Regression', result_list)
+    plot_bar_individual(file_content_list,'LR_Breast_Cancer_w_combination', 'Breast Cancer', 'Binomial Logistic Regression', result_list)
+    plot_bar_individual(file_content_list,'LR_Spam_Email_w_combination', 'Spam Email', 'Binomial Logistic Regression', result_list)
+    plot_bar_individual(file_content_list,'LR_Water_Potability_w_combination', 'Water Potability', 'Binomial Logistic Regression', result_list)
